7299b2bd0de2cee6524ea9c327d3c16dacff84c8
orcunb <orcunbirgul@gmail.com>
2019-01-26 12:24:08 +0300

    basicproject1 file added and done some bug fixes

---
+++ BasicProject1.py
@@ -0,0 +1,3 @@
+mil = 0.621371192
+num1 = int(input("kaç kilometreyi çevirmek istiyorsunuz ---> "))
+print(str(num1) + " km ---> "+ str(num1 * mil) + " mil eder")
--- Lists.py
+++ Lists.py
@@ -1,7 +1,20 @@
-students = ["ali","veli","ahmet"]
+students = ["ali", "veli", "ahmet"]
 
 students.append("ayse")
 students.remove("ahmet")
 
 print(students[1])
 print(students)
+
+# lists constructor
+cities = list(("istanbul", "Rio", "ankara", "Rio"))
+print(str(len(cities)) + " " + str(cities))
+
+# other functions
+
+# print(students.clear()) tüm diziyi siler
+
+print("Rio sayısı : "+ str(cities.count("Rio")))
+cities.pop(3)
+cities.insert(0,"Berlin")
+print(cities)
