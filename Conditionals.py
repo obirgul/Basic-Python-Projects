92425f7d8f5fb4fc7e182675d38ffc03e0b2237a
orcunb <orcunbirgul@gmail.com>
2019-01-28 11:32:25 +0300

    dict and cond. files added

---
+++ Conditionals.py
@@ -0,0 +1,11 @@
+lights = ["green","yellow","red"]
+
+currentlight=lights[1]
+print(currentlight)
+
+if currentlight=="green" :
+    print("go!")
+elif currentlight=="yellow" :
+    print("ready")
+elif currentlight=="red" :
+    print("wait")
---
+++ Data Structures/Dictionaries.py
@@ -0,0 +1,14 @@
+Sozluk = {
+    "kitap" : "book",
+    "kalem" : "pencil"
+}
+
+Sozluk2 = dict(notebook = "defter", engineer = "mühendis")
+
+Sozluk["masa"] = "table"
+Sozluk["kitap"] = "book1"
+
+print(Sozluk)
+print(Sozluk2)
+print(len(Sozluk))
+print(len(Sozluk2))
