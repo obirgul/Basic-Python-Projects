2feb4c5c9b80e97a6e8bd07b4958022962dd598d
orcunb <orcunbirgul@gmail.com>
2019-02-08 11:57:27 +0300

    bugs fixed

--- Projects/Calculator/main.py
+++ Projects/Calculator/main.py
@@ -2,11 +2,11 @@ import MathamaticalMethods as math
 
 print("YAPMAK İSTEDİĞİNİZ İŞLEMİ BOŞLUK VEREREK YAZIN")
 print("örnek işlem 24 / 6 ")
-a = str(input())
+a = str(input("---->>> "))
 if a.split()[1] == "+":
     math.topla(a.split()[0],a.split()[2])
 
-elif a.split()[1] == "*" :
+elif a.split()[1] == "*":
     math.carp(a.split()[0],a.split()[2])
 
 elif a.split()[1] == "/":
